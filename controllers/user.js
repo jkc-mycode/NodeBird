994e12fa1afe623434a8b82a185cfe3a3729b948
jkc-mycode <jkc9709@naver.com>
2024-02-13 22:29:37 +0900

    Add unfollw (2024.02.13)

--- controllers/user.js
+++ controllers/user.js
@@ -17,6 +17,17 @@ exports.follow = async (req, res, next) => {
     }
 };
 
-exports.unfollow = (req, res, next) => {
-
+exports.unfollow = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ where: { id: req.user.id } });
+        if (user) {
+            await user.removeFollowing(parseInt(req.params.id, 10));
+            res.redirect('/');
+        } else {
+            res.status(404).send('no user');
+        }
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 };
